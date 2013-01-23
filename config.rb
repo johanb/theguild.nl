234b4978d521a45cd9ea1d23904aa1f03d64e6a0
Ariejan de Vroom <ariejan@ariejan.net>
2013-01-23 11:26:39 +0100

    Config; CNAME

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/theguild/"
+http_images_path = "/theguild/images"
+http_fonts_path = "/theguild/fonts"
+css_dir = "public/theguild/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
