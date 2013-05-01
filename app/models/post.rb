0e58c7a2d1302434a7050e57536b542c7275f49c
Arjan van der Gaag <arjan@kabisa.nl>
2013-05-01 22:24:52 +0200

    Add basic post model validations
    We want to make sure our Post#summary attributes does not get to long,
    so we add a validation for it. The rest of post validations is left for
    later feature implementations.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ActiveRecord::Base
+  validates :title, :summary, :body, presence: true
+  validates :title, :summary, length: { maximum: 250 }
 end
--- db/migrate/20130501165715_create_posts.rb
+++ db/migrate/20130501165715_create_posts.rb
@@ -1,10 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string :title
-      t.string :summary
-      t.text :body
-
+      t.string :title,   null: false
+      t.string :summary, null: false
+      t.text :body,      null: false
       t.timestamps
     end
   end
