4009a25501288819f73abd47c80543870c565e40
Pascal Widdershoven <pascal@kabisa.nl>
2013-05-02 16:07:52 +0200

    Extract 'No events' msg to hamlified partial

--- spec/features/events/browse_events_spec.rb
+++ spec/features/events/browse_events_spec.rb
@@ -7,7 +7,7 @@ describe 'Listing events' do
     it 'displays a message when no events are available' do
       visit '/'
       within 'section.events' do
-        expect(page).to have_content('No events')
+        expect(page).to have_content('There are no events yet.')
       end
     end
   end
