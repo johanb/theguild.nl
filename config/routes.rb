8658d2f26922297e091e09fbb63280944a83c767
Arjan van der Gaag <arjan@kabisa.nl>
2013-05-02 16:11:25 +0200

    Provide posts in an atom feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Guild::Application.routes.draw do
   resources :blog, controller: 'posts', except: [:index], as: 'posts'
+  get '/blog.atom' => 'posts#index', format: 'atom'
   root to: "posts#index"
 end
